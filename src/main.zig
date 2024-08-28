e4415e19a8d28628ec9a43aba48adb027e154f85
zewenn <takaczsoli@gmail.com>
2024-08-29 00:46:02 +0200

    Start with raylib-zig

---
+++ src/main.zig
@@ -0,0 +1,45 @@
+// raylib-zig (c) Nikolas Wipper 2023
+
+const rl = @import("raylib");
+const os = @import("std").os;
+const fs = @import("std").fs;
+
+pub fn main() !void {
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    defer rl.closeWindow(); // Close window and OpenGL context
+
+    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    var textr = rl.loadImage("/Users/zoltantakacs/_code/zig/testproj/src/assets/player_left_0.png");
+    rl.imageResizeNN(&textr, 160, 160);
+
+    // Main game loop
+    while (!rl.windowShouldClose()) { // Detect window close button or ESC key
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        const x = rl.loadTextureFromImage(textr);
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(rl.Color.white);
+
+        // rl.drawRectangle(0, 0, 100, 100, rl.Color.black);
+
+        rl.drawTexture(x, 0, 0, rl.Color.white);
+
+        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
+        //----------------------------------------------------------------------------------
+    }
+}
